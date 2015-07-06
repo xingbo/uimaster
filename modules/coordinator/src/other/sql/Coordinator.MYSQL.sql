f4b6854b0457364a245e9a7e762a2ab2889b31f4
shaolin <shao.lin@live.com>
2015-07-07 00:01:02 +0800

    update build

--- modules/coordinator/src/other/sql/Coordinator.MYSQL.sql
+++ modules/coordinator/src/other/sql/Coordinator.MYSQL.sql
@@ -2,7 +2,7 @@
 -- Create SQL Generated               --
 --                                    --
 -- Please DO NOT modify !!            --
--- Generated on 2015-06-04 10:37:14   --
+-- Generated on 2015-07-06 11:56:46   --
 #---------------------------------------
 
 USE <database name>;
